197e0569a9251cecc90e2cc807813414b549bab2
Anton <petuhova805@gmail.com>
2024-03-10 15:39:27 +0300

    feat: show components

--- voting-dapp-client/src/components/voting/Voting.jsx
+++ voting-dapp-client/src/components/voting/Voting.jsx
@@ -7,14 +7,15 @@ import Info from "../info/Info";
 
 const Voting = ({ provider, signer }) => {
 	const [selectedBlock, setSelectedBlock] = useState(null);
+
 	return (
 		<div className='voting__grid'>
 			<div className='voting__grid-menu'>
 				<div className='menu__flex'>
 					<button className='menu__home-image-button'
-							onClick={() => setSelectedBlock(Home)}/>
+									onClick={() => setSelectedBlock(<Home/>)}/>
 					<button className='menu__profile-image-button'
-							onClick={() => setSelectedBlock(Profile)}/>
+							onClick={() => setSelectedBlock(<Profile signer={signer} provider={provider}/>)}/>
 					<button className='menu__search-image-button' onClick={() => setSelectedBlock(Search)}/>
 					<button className='menu__info-image-button'
 							onClick={() => setSelectedBlock(Info)}/>
